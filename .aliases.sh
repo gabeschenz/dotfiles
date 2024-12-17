5e41d1df33145a2851920c14cdb28173d86164ef
GabeSchenz <gabe.schenz@allergan.com>
2024-12-17 16:32:54 -0600

    add allergan email; rebase on dev

--- .aliases.sh
+++ .aliases.sh
@@ -4,13 +4,11 @@ if /usr/bin/which -s nvim; then
 	alias vim=nvim
 fi
 
-if /usr/bin/which -s gls; then 
-  alias ls='gls --color=always -F --group-directories-first'
-fi
-
+alias ls='gls --color=always -F --group-directories-first'
 alias ll='ls -l --human-readable --dereference-command-line'
 alias la='ll -A'
 alias ltr='ll -tr'
 alias latr='la -tr'
 
 alias config='/usr/bin/git --git-dir=/Users/schengx/.cfg/ --work-tree=/Users/schengx'
+alias lazy-config='/opt/homebrew/bin/lazygit --git-dir=/Users/schengx/.cfg/ --work-tree=/Users/schengx'
